929c771f60d2d118bff4d44237045e467c81fe6c
Álvaro Fernandez <93alfdez@gmail.com>
2021-12-13 13:24:47 +0100

    descuentos 131221

--- taller #2/descuentos.js
+++ taller #2/descuentos.js
@@ -1,3 +1,78 @@
-function PrecioDescuento(precio,descuento) {
-    return (precio * (100 - descuento)) / 100
+// const precioOriginal = 100;
+// const descuento = 15;
+
+// const porcentajePrecioConDescuento = 100 - descuento;
+
+// const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100;
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento,
+// });
+
+function CalcularPrecioConDescuento(precio,descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+   
+    return precioConDescuento;
+
 }
+
+function ButtonPrecioConDescuento() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = CalcularPrecioConDescuento(priceValue,discountValue);
+
+    const ResultP = document.getElementById("ResultP");
+
+    ResultP.innerText = "El precio con descuento son: " + precioConDescuento + " €";
+}
+// (precio * (100 - descuento)) / 100
+
+
+
+function CalcularPrecioConDescuento(precio,descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickPrecioDescuento() {
+    const cupones = ["CODE10","CODE20","CODE30"];
+
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCupon = document.getElementById("InputCupon");
+    const cuponValue = inputCupon.value;
+
+    let descuento;
+
+    switch (cuponValue) {
+        case "CODE10":
+            descuento = 10;
+            break;
+        case "CODE20":
+            descuento = 20;
+            break;
+        case "CODE30":
+                descuento = 30;
+                break;
+        default:
+            descuento = 0;
+            break;
+    }
+
+    const precioConDescuento = CalcularPrecioConDescuento(priceValue,descuento);
+
+    const ResultP = document.getElementById("ResultP");
+
+    ResultP.innerText = "El precio con descuento son: " + precioConDescuento + " €";
+}
+
